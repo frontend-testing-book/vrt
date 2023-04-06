c2d2deab7e056c652331278c5cbb2fe424c4e1df
takefumi-yoshii <yoshiitakefumidw@gmail.com>
2023-04-06 21:46:15 +0900

    chore: add example

---
+++ .storybook/preview.js
@@ -0,0 +1,13 @@
+import { withScreenshot } from "storycap";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [withScreenshot];
---
+++ stories/Button.stories.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button } from "./Button";
+
+export default {
+  title: "Example/Button",
+  component: Button,
+};
+
+export const Default = {
+  args: {
+    label: "Button",
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: "Button",
+  },
+};
+
+export const Large = {
+  args: {
+    size: "large",
+    label: "Button",
+  },
+};
+
+export const Small = {
+  args: {
+    size: "small",
+    label: "Button",
+  },
+};
